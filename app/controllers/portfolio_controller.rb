7d67d8c218fbfabc6e934143a03ac2e2e9b547e7
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-09 19:38:50 -0600

    hash created

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,6 +1,28 @@
 class PortfolioController < ApplicationController
 
 	def primera
-		@numero = rand(100..10001)
+		@portfolio = {
+			info: {
+				name: "Edgar Paz",
+				address: "Falsa #123",
+				age: 28
+			},
+			studies: {
+				university: "Universidad de España",
+				grade_univ: "Lic. en Derecho",
+				master: "Universidad de Barcelona",
+				grade_master: "Maestría en Dirección de Empresas Deportivas"
+			},
+			work: {
+				first: {
+					title: "Abogado en pleitos",
+					company: "Despacho de Abogados Españoles"
+				},	
+				second: {
+					title: "Coordinador de Marketing",
+					company: "Omnilife"
+				}
+			}
+		}
 	end
 end	
